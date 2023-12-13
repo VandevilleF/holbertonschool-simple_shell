edd9ae8501dc1e813296506290e7740a99f96b8e
Florent1711 <graphit01@hotmail.com>
2023-12-13 06:17:39 -0800

    fix indentations and code

--- which.c
+++ which.c
@@ -3,31 +3,35 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    char *path = getenv("PATH");
-    char *dir;
-    char *p;
-    char filename[1000];
-    int found = 0;
+int main(int argc, char *argv[])
+{
+	char *path = getenv("PATH");
+	char *dir;
+	char *p;
+	char filename[1000];
+	int found = 0;
 
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
-        return 1;
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s filename\n", argv[0]);
+		return 1;
+	}
 
-    while ((dir = strsep(&path, ":")) != NULL) {
-        sprintf(filename, "%s/%s", dir, argv[1]);
-        if (access(filename, F_OK) == 0) {
-            printf("%s\n", filename);
-            found = 1;
-            break;
-        }
-    }
+	while ((dir = strsep(&path, ":")) != NULL)
+	{
+		sprintf(filename, "%s/%s", dir, argv[1]);
+		if (access(filename, F_OK) == 0)
+		{
+			printf("%s\n", filename);
+			found = 1;
+			break;
+		}
+	}
 
-    if (!found) {
-        fprintf(stderr, "%s: command not found\n", argv[1]);
-        return 1;
-    }
-
-    return 0;
+	if (!found)
+	{
+		fprintf(stderr, "%s: command not found\n", argv[1]);
+		return 1;
+	}
+	return 0;
 }
