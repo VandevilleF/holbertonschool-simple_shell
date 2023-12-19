a16354d93355564e2f2302afe407b44bf14daa68
VandevilleF <flo.vandeville@gmail.com>
2023-12-19 14:21:36 -0800

    Fix memory leak FINALLY ! and add free_tok_array foction in simple_shell file for less line in main

--- simple_shell.c
+++ simple_shell.c
@@ -1,6 +1,24 @@
 #include "main.h"
 
 /**
+ * free_tok_array - free array of token
+ * @tok_array: array where token are stored
+ * Retrun: Nothing
+ */
+
+void free_tok_array(char ** tok_array)
+{
+	int i;
+
+	if (tok_array != NULL)
+	{
+		for (i = 0; tok_array[i] != NULL; i++)
+		free(tok_array[i]);
+		free(tok_array);
+	}
+}
+
+/**
  * main - main
  * @ac: void
  * @av: void
@@ -9,45 +27,54 @@
  */
 
 
-int main(int ac, char *av, char **env)
+int main(int ac, char **av, char **env)
 {
 	char *input = NULL;
 	size_t bufsize = 0;
 	char *token, **tok_array;
-	int j, i = 0;
+	int i;
+	ssize_t char_read;
+
 	(void)ac, (void)av;
 
 	while (1)
 	{
 		printf("$ ");
-		ssize_t char_read = getline(&input, &bufsize, stdin);
+		char_read = getline(&input, &bufsize, stdin);
 
-		if (char_read == -1)
+		if (char_read != 1)
 		{
-			perror("Error reading line");
-			return (-1);
-		}
-		tok_array = malloc(sizeof(char *) * 1024);
+			tok_array = malloc(sizeof(char *) * 20);
 
-		if (tok_array == NULL)
-		{
-			free(tok_array);
-			return (-1);
+			if (tok_array == NULL)
+			{
+				free(input);
+				exit(EXIT_FAILURE);
+			}
+			token = strtok(input, " \n\t");
+
+			i = 0;
+
+			while (token != NULL)
+			{
+				tok_array[i] = strdup(token);
+				token = strtok(NULL, " \n\t");
+				i++;
+			}
+			tok_array[i] = NULL; /*Add NULL at the end*/
+
+			if (strcmp(tok_array[0], "exit") == 0)
+			{
+				free_tok_array(tok_array);
+				free(input);
+				break;
+			}
+			execute_command(tok_array[0], tok_array, env);
+
+			free_tok_array(tok_array);
 		}
-		token = strtok(input, " \n\t");
-		while (token != NULL)
-		{
-			tok_array[i] = strdup(token);
-			token = strtok(NULL, " \n\t");
-			i++;
-		}
-		tok_array[i] = NULL; /*Add NULL at the end*/
-		execute_command(tok_array[0], tok_array, env);
-		for (j = 0; j < i; j++)
-			free(tok_array[j]);
-		i = 0;
+		free(input);
+		input = NULL;
 	}
-	free(tok_array);
-	free(input);
 	return (0);
 }
